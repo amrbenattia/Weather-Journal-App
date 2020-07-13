32d01d47038a4cf32c49e10c3ea7b40598c611eb
Amr Attia <amr.atyia.ham@gmail.com>
2020-07-14 02:46:10 +0900

    second commit

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.static('website'));
 
 
 // Setup Server
-const port = process.env.PORT || 3000;
+const port =  process.env.PORT || 3000; 
 const server = app.listen(port, listening);
  function listening(){
     console.log(`running on localhost: ${port}`);
